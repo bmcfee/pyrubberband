62d62fc967a03c4155a322414096915bf08ad94d
Brian McFee <brian.mcfee@nyu.edu>
2015-10-19 09:35:23 -0400

    added time stretch test

--- tests/test_pyrb.py
+++ tests/test_pyrb.py
@@ -2,3 +2,18 @@
 # -*- encoding: utf-8 -*-
 
 import pyrubberband
+import numpy as np
+
+def test_stretch():
+
+    sr = 8000
+    duration = 5
+    y = np.zeros(sr * duration)
+
+    def __test(rate):
+        y_s = pyrubberband.time_stretch(y, sr, rate=rate)
+
+        assert np.allclose(rate * len(y_s), len(y))
+
+    for rate in [0.5, 1.0, 2.0]:
+        yield __test, rate
