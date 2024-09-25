b5091c8568b8f2537651d4a9cf5159455df13c0b
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-09-25 18:45:51 +0900

    Fix version importing problem in python 3.6

--- pyrubberband/__init__.py
+++ pyrubberband/__init__.py
@@ -3,5 +3,5 @@
 # CREATED:2015-03-02 11:50:46 by Brian McFee <brian.mcfee@nyu.edu>
 '''A python wrapper for rubberband'''
 
-from .pyrb import *
 from .version import version as __version__
+from .pyrb import *
